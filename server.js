cbed263da817ee358fcbd5c83ee27fe7470c2f88
nicholasjamessmith <njsmith6@gmail.com>
2023-06-13 14:55:21 -0400

    drinks database connected and visible in browswer

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 const express = require("express")
 //Set 'express()' to a variable
 const app = express()
+//require database ('drinks' object)
+const drinks = require("./models/drinks")
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!")
+})
+
+app.get("/drinks", (req, res) => {
+  res.send(drinks)
 })
 
 //Set app to listen to port and include a console.log() to tell when server is running
